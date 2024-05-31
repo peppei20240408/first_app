b5b457450ba49a6afd0631c3d341a19ba00dcd1b
peppei20240408 <ippei.kanoh@gmail.com>
2024-05-31 10:58:26 +0900

    Gitの使い方練習_indexにaddする

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
+  get 'posts/new', to: 'posts#new' #Git 練習コメント
   post 'posts', to: 'posts#create'
 end
